bbaea5ce80885ed3a8fdecde4ad12c35d012968a
codebull707 <codebull707@gmail.com>
2020-12-15 19:52:59 +0530

    Made the UI look good. DARK THEME!🤩

--- app.js
+++ app.js
@@ -5,25 +5,40 @@ const app = Vue.createApp({
             name_error: false,
             messages: [],
             curr_message: '',
-            socket: io('http://localhost:3000')
+            color: '',
+            socket: io('http://localhost:3000'),
+            dark_mode: true
+        }
+    },
+    watch: {
+        dark_mode: function () {
+            document.body.classList.toggle('bg-dark')
+            var tmp = document.getElementById('card').classList
+            tmp.toggle('bg-dark')
+            tmp.toggle('text-light')
         }
     },
     methods: {
         sendMessage() {
-            this.socket.emit('message', { content: this.curr_message, author: this.author })
+            this.socket.emit('message', { content: this.curr_message, author: this.author, color: this.color })
             this.curr_message = ''
         },
         changeName() {
             this.name_error = false
             this.socket.emit('changename', { name: this.author })
-        }
+        },
     },
     mounted() {
         this.socket.on('connected', (data) => {
             this.author = data.name
+            this.color = data.color
         }),
             this.socket.on('message', (data) => {
                 this.messages.push(data.message)
+                this.$nextTick(() => {
+                    container = document.getElementById('messages');
+                    container.scrollTop = container.scrollHeight;
+                })
             }),
             this.socket.on('invalid_name', (data) => {
                 this.author = data.orig_name
