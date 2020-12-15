27bed856b00504694ed2a7030b94856a62698b7a
codebull707 <codebull707@gmail.com>
2020-12-15 14:22:40 +0530

    basic chat app. UI sucks tho...🕸

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             author: '',
+            name_error: false,
             messages: [],
             curr_message: '',
-            socket: io('http://localhost:4000')
+            socket: io('http://localhost:3000')
         }
     },
     methods: {
@@ -12,6 +13,10 @@ const app = Vue.createApp({
             this.socket.emit('message', { content: this.curr_message, author: this.author })
             this.curr_message = ''
         },
+        changeName() {
+            this.name_error = false
+            this.socket.emit('changename', { name: this.author })
+        }
     },
     mounted() {
         this.socket.on('connected', (data) => {
@@ -19,6 +24,10 @@ const app = Vue.createApp({
         }),
             this.socket.on('message', (data) => {
                 this.messages.push(data.message)
+            }),
+            this.socket.on('invalid_name', (data) => {
+                this.author = data.orig_name
+                this.name_error = true
             })
     },
 })
